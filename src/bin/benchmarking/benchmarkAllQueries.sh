7bba09ba110d2fcedcd717d108398c8939a4687c
doogyb <samueldoogan@gmail.com>
2021-12-16 14:59:18 +0000

    Automate uploading of benchmark results
    This PR introduces the automation of the uploading of benchmark results
    through a combination of a JS script, SSH comands to be run on
    the Virtuoso server, and a post hook command that initiates
    the process once Iguana has finished executing.
    
    closes #33

--- src/bin/benchmarking/benchmarkAllQueries.sh
+++ src/bin/benchmarking/benchmarkAllQueries.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-node src/bin/all/concatenateQueries.mjs \
+node src/bin/queries/concatenateQueries.mjs \
     -i graphs/performance graphs/quality -o src/benchmarking \
 && cd src/benchmarking && java -jar iguana-3.3.0.jar config.yml 
