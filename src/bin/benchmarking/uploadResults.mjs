9a9f9707269e0beccfaedc3087432196454f9146
doogyb <samueldoogan@gmail.com>
2021-12-23 18:06:49 +0100

    Write script to stop/start and benchmark DBs
    This PR introduces a bash script which uses `pkill` to stop relevant
    processes, and then starts the different triplestore executables before
    running the benchmarks. It then uploads both sets of results to the same
    named graph in Virtuoso.
    
    closes #41

--- src/bin/benchmarking/uploadResults.mjs
+++ src/bin/benchmarking/uploadResults.mjs
@@ -11,6 +11,17 @@ import {
 	EC2ResultsDir,
 } from '../../node_modules/config.mjs';
 
+program.option(
+	'-f --file <file>',
+	'Result file to upload. If this option is not \
+		specified, the latest file will be uploaded'
+);
+program.option(
+	'-g --graph-name <graphName>',
+	'Optional name of graph to be used for results. \
+		The results IRI prefix of <http://nesta.org.uk/benchmarking/> \
+		will always get prepended to this option'
+);
 program.requiredOption(
 	'-p, --path <path>',
 	'Path to directory benchmark results'
@@ -20,24 +31,30 @@ program.parse(process.argv);
 const options = program.opts();
 
 const main = async () => {
-	const oldPath = await mostRecentBenchmarkFilePath(options.path);
-	const newPath = oldPath.replace('results_', '');
+	const oldFilePath = options.file
+		? path.join(options.path, options.file)
+		: await mostRecentBenchmarkFilePath(options.path);
+	const newFilePath = oldFilePath.replace('results_', '');
 
-	const fileName = path.basename(newPath);
+	const fileName = path.basename(newFilePath);
 	const graphBase = 'http://nesta.org.uk/benchmarking/results';
-	const graphName = `${graphBase}/${fileName.replace('.nt', '')}`;
+	const graphName = options.graphName
+		? `${graphBase}/${options.graphName}`
+		: `${graphBase}/${fileName.replace('.nt', '')}`;
 
-	await fs.rename(oldPath, newPath);
+	// await fs.rename(oldFilePath, newFilePath);
 
-	const uploadCommand = `scp ${newPath} ubuntu@${address}:${EC2ResultsDir}`;
+	const uploadCommand = `scp ${newFilePath} ubuntu@${address}:${EC2ResultsDir}`;
 	const sqlCommand = `ld_dir (\\'${EC2ResultsDir}\\', \\'${fileName}\\', \\'${graphName}\\');\nrdf_loader_run();`;
 	const bashCommand = `ssh ubuntu@${address} "echo $'${sqlCommand}' | isql"`;
 
-	execSync(uploadCommand);
-	execSync(bashCommand);
+	console.log(uploadCommand);
+	console.log(bashCommand);
+	// execSync(uploadCommand);
+	// execSync(bashCommand);
 
-	await fs.copyFile(newPath, `${options.path}/results/${fileName}`);
-	await fs.rm(newPath);
+	// await fs.copyFile(newFilePath, `${options.path}/results/${fileName}`);
+	// await fs.rm(newFilePath);
 };
 
 main();
